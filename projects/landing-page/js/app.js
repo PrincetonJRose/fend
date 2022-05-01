e65237b5fe0aa84ccc3a11744a3b2a5f439fc3e4
Princeton Rose <princetonjrose@gmail.com>
2022-05-01 15:50:08 -0400

    finished up scrolling event

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -13,26 +13,43 @@ const sections = document.getElementsByTagName('section')
 
 
 
-
 let buildNavbar =()=> {
     for ( let section of sections ) {
         let li = document.createElement('li')
-
+        
         let span = document.createElement('span')
         span.classList += 'menu__link'
         span.innerText = section.dataset.nav
         span.dataset.id = section.id
-
+        
         li.appendChild(span)
         navbarList.appendChild(li)
-    };
+    }
+}
+
+document.onscroll = (event)=> {
+    for ( let section of sections ) {
+        checkIfSectionInView( section )
+    }
+}
+
+let checkIfSectionInView =( section )=> {
+    let position = section.getBoundingClientRect()
+    let viewHeight = document.documentElement.clientHeight
+    
+    if ( position.top <= viewHeight && position.top <= 500 && position.top >= 0 ) {
+        if ( !section.className.includes('active') )
+            section.classList += ' active'
+    } else if ( section.className.includes('active') )
+        section.classList.remove('active')
 }
 
 navbarList.onclick = (event)=> {
-    if ( event.target.className.includes('menu__link') )
-        sections[`${event.target.dataset.id}`].scrollIntoView({ behavior: "smooth" })
+    let element = event.target
+    if ( element.className.includes('menu__link') ) {
+        sections[`${element.dataset.id}`].scrollIntoView({ behavior: "smooth" })
+    }
 }
-
 
 buildNavbar()
 
